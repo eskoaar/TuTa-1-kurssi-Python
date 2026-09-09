eaff88b889e62e921e94984cee3df0195381ffd2
eskoaar <aaron.eskola@metropolia.fi>
2026-09-09 22:08:11 +0300

    lisää tehtävät

--- Moduuli 2.6.py
+++ Moduuli 2.6.py
@@ -11,5 +11,3 @@ koodi3 = koodi1_numero1 + koodi1_numero2 + koodi1_numero3
 koodi4 = koodi2_numero1 + koodi2_numero2 + koodi2_numero3 + koodi2_numero4
 print(f"kolmen koodi (0-9): {koodi3}")
 print(f"neljän koodi (1-6): {koodi4}")
-
-6
